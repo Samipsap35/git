4d67feb6b4fd899ffd8132b6664d4c6d243d4524
sabin <sober.sabin@gmail.com>
2021-10-30 15:35:13 +0200

    my first information

---
+++ my.py
@@ -0,0 +1,14 @@
+while True:
+
+    try:
+        x = input('Enter ')
+        if x == 'q':
+            break
+        else:
+
+            print(int(x) ** 2 )
+    except:
+        print('please enter numeric value')
+
+
+print('Bye Bye see you soon')
