f163c4d7e09fc41eabbd814eb5781a642b7c2b04
sabin <sober.sabin@gmail.com>
2021-10-31 16:02:53 +0100

    few lines were added

--- my.py
+++ my.py
@@ -1,14 +1,17 @@
 while True:
 
+
     try:
         x = input('Enter ')
         if x == 'q':
             break
         else:
 
-            print(int(x) ** 2 )
+            print(int(x) ** 2 +1 )
     except:
         print('please enter numeric value')
 
 
 print('Bye Bye see you soon')
+
+# lets add few line
